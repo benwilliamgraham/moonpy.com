6f030560aac28add45c1201a0ac9ad7d33be23c5
blue <benwilliamgraham@gmail.com>
2020-02-29 23:14:43 -0500

    V 1.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,37 @@ class Home extends Component {
     return (
       <div>
         <title>Home | Moonpy</title>
-        <img alt="moonpy" src="images/moonpy.svg" />
-        <h2>Python like never before</h2>
+        <img
+          alt="moonpy"
+          src="images/moonpy.svg"
+          style={{ display: "block", margin: "auto" }}
+        />
+        <h2 style={{ textAlign: "center" }}>Python like never before</h2>
+
+        <hr />
+
+        <h3>
+          Moonpy is a static compiler for Python that is looking to
+          revolutionize the way code is developed.
+        </h3>
+
+        <p>
+          It uses state-of-the-art technology to convert Python code into a
+          machine-level binary that is capable of running up to 300+ times
+          faster than its interpreted equivalent without sacrificing any of what
+          makes Python so incredible.
+        </p>
+
+        <p>
+          If that wasn't enough, running a program using Moonpy uses an average
+          of 38% less memory and eliminates the need for a clunky interpreter.
+        </p>
+
+        <p>
+          Additionally, the static analysis done by Moonpy is able to locate all
+          attribute and type errors in a program, which are constant nuisances
+          of Python programmers.
+        </p>
       </div>
     );
   }
--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,60 @@ class Home extends Component {
     return (
       <div>
         <title>Product | Moonpy</title>
-        Product
+        <h2 style={{ textAlign: "center" }}>Why Moonpy?</h2>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            margin: "auto",
+            maxWidth: "800px"
+          }}
+        >
+          {[
+            {
+              attr: "Fast",
+              img: "images/fast.svg",
+              desc: "Up to 300+ times speedup compared to CPython"
+            },
+            {
+              attr: "Low Overhead",
+              img: "images/low overhead.svg",
+              desc: "Pay only for what you use; no interpreter necessary"
+            },
+            {
+              attr: "Reliable",
+              img: "images/reliable.svg",
+              desc: "Eliminate runtime type and attribute errors"
+            },
+            {
+              attr: "Multiplatform",
+              img: "images/multiplatform.svg",
+              desc: "Run anywhere, even in the browser"
+            }
+          ].map((reason, i) => (
+            <div
+              key={i}
+              className="outline"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "280px",
+                padding: "30px",
+                margin: "10px",
+                textAlign: "center"
+              }}
+            >
+              <h3 style={{ textAlign: "center" }}>{reason.attr}</h3>
+              <img
+                src={reason.img}
+                alt={reason.attr}
+                style={{ width: "150px", margin: "auto" }}
+              />
+              {reason.desc}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
