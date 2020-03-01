1452d39d491ef0c1059bbe695d412745ab04ecba
blue <benwilliamgraham@gmail.com>
2020-02-29 23:15:06 -0500

    Updates

---
+++ precache-manifest.eaf8a2cdfcc0102887e54daf0d0f0da8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a28715f71de1ae984988fab74ef203",
+    "url": "/moonpy/index.html"
+  },
+  {
+    "revision": "a5eb6cb8c970fcdb210b",
+    "url": "/moonpy/static/css/main.39aef9da.chunk.css"
+  },
+  {
+    "revision": "10f457df9e5f0a27984e",
+    "url": "/moonpy/static/js/2.91b9ec68.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/moonpy/static/js/2.91b9ec68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5eb6cb8c970fcdb210b",
+    "url": "/moonpy/static/js/main.20da00d5.chunk.js"
+  },
+  {
+    "revision": "38ab60b7479097746546",
+    "url": "/moonpy/static/js/runtime-main.d9120efc.js"
+  }
+]);
